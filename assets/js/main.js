0011efee4221e1a39a938400ae260f18e9001af5
Mattéo Legagneux <matteoo34@icloud.com>
2021-12-10 15:49:37 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,16 +8,17 @@ const R = {x: 0, y: 0},
 	// Mouse events
 	move = e => {
 		R.x = e.gamma;
-		R.y = -e.beta;
+		// R.y = -e.beta;
+		R.y = 0;
 		/*if (R.x < -360) R.x += 360;
 		if (R.x > 360) R.x -= 360;*/
-		if (R.y < -45) R.y = -45;
-		if (R.y > 45) R.y = 45;
+		// if (R.y < -45) R.y = -45;
+		// if (R.y > 45) R.y = 45;
 		document.querySelector(".test").textContent = `R.y = ${R.y}`;
-		let transform = `rotateX(${R.y + 90}deg) rotateY(${-R.x}deg)`;
+		let transform = `rotateX(${R.y}deg) rotateY(${-R.x}deg)`;
 		cube.style["-webkit-transform"] = transform;
 		cube.style.transform = transform;
-		shadowFacing(R.y)
+		// shadowFacing(R.y)
 	},
 	/*move = e => {
 		if (!e.clientX) {
