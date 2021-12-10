a70189479756d81cd4ab01c5bbf55957dcea4572
Mattéo Legagneux <matteoo34@icloud.com>
2021-12-10 16:03:11 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ const R = {x: 0, y: 0},
 		let transform = `rotateX(${-R.y}deg) rotateY(${-R.x}deg)`;
 		cube.style["-webkit-transform"] = transform;
 		cube.style.transform = transform;
-		shadowFacing(R.y)
+		shadowFacing(R.y - 90)
 	},
 	shadowFacing = ry => {
 		// Apply a shadow to the faces
