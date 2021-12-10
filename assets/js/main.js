a1070c187d98ba1e68c137556c42369b42db547d
Mattéo Legagneux <matteoo34@icloud.com>
2021-12-10 16:26:59 +0100

    VR test #10

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ const R = {x: 0, y: 0}, // Cube rotation values
 	move = e => {
 		R.x = e.gamma;
 		R.y = e.beta;
-		document.querySelector(".test").textContent = R.y.toFixed(P);
+		document.querySelector(".test").textContent = -R.y.toFixed(P);
 		let transform = `rotateX(${-R.y.toFixed(P)}deg) rotateY(${-R.x.toFixed(P)}deg)`;
 		cube.style["-webkit-transform"] = transform;
 		cube.style.transform = transform;
@@ -16,7 +16,7 @@ const R = {x: 0, y: 0}, // Cube rotation values
 		// Apply a shadow to the faces
 		cube.children[0].style.boxShadow = `inset 0 0 0 100px rgba(255, 255, 255, ${Math.abs((180 - Math.abs(ry)) / 180 - 0.5)})`;
 		cube.children[1].style.boxShadow = `inset 0 0 0 100px rgba(0, 0, 0, ${Math.abs((180 - Math.abs(ry)) / 180 - 0.5)})`;
-		let rgb = (R.y >= 0) ? "255, 255, 255" : (R.y < 0) ? "0, 0, 0" : undefined;
+		let rgb = (ry >= 0) ? "255, 255, 255" : (ry < 0) ? "0, 0, 0" : undefined;
 		if (rgb) {
 			[cube.children].forEach(face => {
 				face.style.boxShadow = `inset 0 0 0 100px rgba(${rgb}, ${Math.abs((90 - Math.abs(ry)) / 180 - 0.5)})`
