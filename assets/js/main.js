548b5399860de906c1e054032e17c3fc3c07cfe7
Mattéo Legagneux <matteoo34@icloud.com>
2021-12-10 16:01:45 +0100

    VR test #7

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,15 @@
 // Cube rotation values
 const R = {x: 0, y: 0},
-	w2 = window.innerWidth / 2, // Window width / 2
-	h2 = window.innerHeight / 2, // Window height / 2
-	// Parameters
-	S = 4, // Sensitivity (higher number = lower sens)
-	P = 2, // Smooth motion (higher number = smoother motion)
 	// Mouse events
 	move = e => {
 		R.x = e.gamma;
-		R.y = -e.beta;
-		// if (R.y < -45) R.y = -45;
-		// if (R.y > 45) R.y = 45;
-		document.querySelector(".test").textContent = `R.x = ${R.x}<br>R.y = ${R.y}`;
-		let transform = `rotateX(${R.y}deg) rotateY(${-R.x}deg)`;
+		R.y = e.beta;
+		document.querySelector(".test").innerHTML = `${R.x}<br>${R.y}`;
+		let transform = `rotateX(${-R.y}deg) rotateY(${-R.x}deg)`;
 		cube.style["-webkit-transform"] = transform;
-		cube.style.transform = transform;
-		// shadowFacing(R.y)
-	},
-	/*move = e => {
-		if (!e.clientX) {
-			// Touch screen
-			R.now.x = -w2 + e.touches[0].clientX;
-			R.now.y = h2 + -e.touches[0].clientY
-		} else {
-			R.now.x = -w2 + e.clientX;
-			R.now.y = h2 + -e.clientY
-		}
-		R.x = ((R.now.x - R.on.x) / S) + R.old.x;
-		R.y = ((R.now.y - R.on.y) / S) + R.old.y;
-		if (R.x < -360) R.x += 360;
-		if (R.x > 360) R.x -= 360;
-		if (R.y < -90) R.y = -90;
-		if (R.y > 90) R.y = 90;
-		// Cube motion
-		let transform = `rotateX(${R.y.toFixed(P)}deg) rotateY(${R.x.toFixed(P)}deg)`;
-		cube.style["-webkit-transform"] = transform;
-		cube.style["-ms-transform"] = transform;
 		cube.style.transform = transform;
 		shadowFacing(R.y)
-	},*/
+	},
 	shadowFacing = ry => {
 		// Apply a shadow to the faces
 		cubeTop.style.boxShadow = `inset 0 0 0 100px rgba(255, 255, 255, ${Math.abs((180 - Math.abs(ry)) / 180 - 0.5)})`;
@@ -63,5 +34,5 @@ const R = {x: 0, y: 0},
 	cubeBack = cube.children[3],
 	cubeLeft = cube.children[4],
 	cubeRight = cube.children[5];
-// Event listeners
+// Device orientation change event
 if (DeviceOrientationEvent) addEventListener("deviceorientation", move)
